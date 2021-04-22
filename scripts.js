47462a1b69ea646e4cb561e8070a3443dd563ace
baltaz <baltazar.crstn@gmail.com>
2021-04-22 13:10:18 -0300

    delete feature added

--- scripts.js
+++ scripts.js
@@ -8,81 +8,69 @@ const sortableConfig = {
     group:'board',
     sort: true,
     animation: 200,
-    easing: "ease-in",
 }
 columns.forEach(e=>{
     Sortable.create(e,sortableConfig);
 })
 
-/* const todo = document.querySelector(".dragable-zone");
-const doing = document.querySelector(".doing");
-const done = document.querySelector(".done");
 
 
-let sortableToDo = Sortable.create(todo,sortableConfig);
-let sortableDoing = Sortable.create(doing,sortableConfig);
-let sortableDone = Sortable.create(done,sortableConfig); */
 
 
-/* 
 cards.forEach(card=>{
-    card.addEventListener('dragstart',e=>{
-        card.classList.add("dragging");
-    })
-    card.addEventListener('dragend',e=>{
-        card.classList.remove("dragging");  
-    })
     card.addEventListener('mousedown',e=>{
-        mouseDown=true;
+        console.log(card.id);
+        mouseDown = true;
         setTimeout(()=>{
             if(mouseDown){
                 card.classList.add("holding");
+            setTimeout(()=>{
+                if(mouseDown){
+                    card.remove();
+                }
+            },2000)
             }
-        },1000);
+        },500);
     })
     card.addEventListener('mouseup',e=>{
-        console.log("mouse up");
+        console.log(e.target.id);
         card.classList.remove("holding")
         mouseDown=false
     });
+    card.addEventListener('mouseleave', e=>{
+        card.classList.remove("holding")
+        mouseDown=false
+    })
 })
 
-columns.forEach(column=>{
+/* columns.forEach(column=>{
     column.addEventListener('dragover', e=>{
         e.preventDefault();
         const card = document.querySelector('.dragging')
         column.appendChild(card);
     })
-})
+}) */
 
-let addButton = document.querySelector(".add-button");
-let modal = document.querySelector(".modal");
-let titleField = document.querySelector(".title-field");
-let descriptionField = document.querySelector(".description-field");
+/* 
+// Get a reference to an element.
+var square = document.getElementById('0');
 
-const hideElement = e => e.classList.remove("show");
-const showElement = e => {
-    console.log(e.childNodes);
-    e.childNodes[1].classList.add("show");
-    e.style.display="block";
-}
+// Create an instance of Hammer with the reference.
+var hammer = new Hammer(square);
 
-addButton.onclick = ()=>{
-    showElement(modal);
-    descriptionField.value="";
-    titleField.value="";
-    saveButton.classList.remove("active")
-};
-modal.onclick = e => {if(e.target==modal) hideElement(modal)}; */
+// Subscribe to a quick start event: press, tap, or doubletap.
+// For a full list of quick start events, read the documentation.
+hammer.on('press', function(e) {
+  e.target.classList.toggle('holding');
+  console.log("You're pressing me!");
+  console.log(e);
+});
+
+ */
 
 
 
-
-
+//delete keep pressing (iphone effect)
 //modal de add con animations
 //create cards
-//cambiar ubicación al soltar
-//drag and drop to delete
 //access to edit
-//mobile touch
-//delete keep pressing (iphone effect)
